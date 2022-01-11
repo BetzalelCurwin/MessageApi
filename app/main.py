5da8ecd94fa10baec7ab367cc52fff7c820707a0
BetzalelCurwin <betz98@gmail.com>
2022-01-11 19:08:46 +0200

    added app

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'temporary-Secret-key-will-change-later'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messageapi.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 jwt = JWTManager(app)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from app.main import app
+from app.main import app, db, User
 
-if __name__ ==  "__main__":
+db.create_all()
+
+user1 = User(email='email1')
+user2 = User(email='email2')
+db.session.add(user1)
+db.session.add(user2)
+db.session.commit()
+
+
+if __name__ == "__main__":
     app.run()
