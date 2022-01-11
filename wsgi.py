7ed60b973132a0841042b693fb2386963551a176
BetzalelCurwin <betz98@gmail.com>
2022-01-11 19:23:33 +0200

    fixed bug

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 from app.main import app, db, User
 
-db.create_all()
-
-user1 = User(email='email1')
-user2 = User(email='email2')
-db.session.add(user1)
-db.session.add(user2)
-db.session.commit()
+try:
+    db.create_all()
+    user1 = User(email='email1')
+    user2 = User(email='email2')
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.commit()
+except Exception:
+    pass
 
 
 if __name__ == "__main__":
